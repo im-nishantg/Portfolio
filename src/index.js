e99f3a87a00c4ba72d50de82c39763a12a2ec933
im-nishantg <96784121+im-nishantg@users.noreply.github.com>
2022-05-10 12:22:19 +0530

    Intial Commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import 'bootstrap/dist/css/bootstrap.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+)
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// #FFF8F3
+// #A3E4DB
+// #1C6DD0
+// #FED1EF
+
+
+// font-family: 'Libre Franklin', sans-serif;
+// font-family: 'Roboto', sans-serif;
